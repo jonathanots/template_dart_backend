60a72da2f7ec2327d711872125e7f5b49cdfb783
Jonathan Souza <jsouza@yoobot.com.br>
2021-12-30 16:35:59 -0300

    [ADD] Created extractor usecase for create an entity

--- example/extractor/extractor_module.dart
+++ example/extractor/extractor_module.dart
@@ -1,11 +1,14 @@
 import 'package:shelf_modular/shelf_modular.dart';
 
+import 'usecases/create_c_usecase.dart';
 import 'usecases/create_entity.dart';
 
 class ExtractorModule extends Module {
   @override
   List<ModularRoute> get routes => [
-        Route.get('/:module',
+        Route.get('/entity/:module',
             (ModularArguments args) => ExtractorCreateEntity().call(args)),
+        Route.get('/create/:module',
+            (ModularArguments args) => ExtractorCreateUsecase().call(args)),
       ];
 }
--- example/extractor/usecases/create_c_usecase.dart
+++ example/extractor/usecases/create_c_usecase.dart
@@ -14,7 +14,7 @@ abstract class IExtractorCreateCUsecase {
   Future<void> _createFile(Map<String, dynamic> source, String moduleName);
 }
 
-class ExtractorCreateEntity implements IExtractorCreateCUsecase {
+class ExtractorCreateUsecase implements IExtractorCreateCUsecase {
   @override
   Future<Response> call(ModularArguments args) async {
     final app = Modular.get<AppController>();
@@ -51,30 +51,62 @@ class ExtractorCreateEntity implements IExtractorCreateCUsecase {
 
     var content = "";
 
+    content += "import 'dart:async';\n";
+    content += "import 'dart:convert';\n";
     content +=
         "import 'package:framework/core/utils/json_serializable.dart';\n";
+    content += "import 'package:shelf/shelf.dart';\n";
+    content += "import 'package:shelf_modular/shelf_modular.dart';\n";
+    content += "import '../../shared/controllers/app_controller.dart';\n";
+    content += "import '../../shared/utils/response.dart';\n";
+    content += "import '../${moduleName.toLowerCase()}_entity.dart';\n";
 
-    content += "import 'package:uuid/uuid.dart';\n";
+    content += "\nabstract class ICreate${className}Usecase {\n";
+    content += "\tFutureOr<Response> call(ModularArguments args);\n";
+    content += "}\n";
 
-    content += "\nclass $className with JsonSerializable {\n";
+    content += "\n";
 
-    content += "\tlate String? id;\n";
+    content +=
+        "class Create${className}Usecase implements ICreate${className}Usecase {\n";
+    content += "\t@override\n";
+    content += "\tFutureOr<Response> call(ModularArguments args) async {\n";
+    content += "\t\tfinal app = Modular.get<AppController>();\n";
 
-    var constructorFields = "";
+    content += "\t\t\n";
 
-    for (var field in source.entries) {
-      if (field.key == "_id") continue;
-      content += "\tfinal ${field.value.runtimeType} ${field.key};\n";
-      constructorFields += "required this.${field.key},";
-    }
+    content += "\t\ttry{\n";
+    content += "\t\t\tawait app.config.initMongo();\n";
+    content += "\t\t\tvar mongo = app.config.mongo!.conn!;\n";
+    content += "\t\t\t\n";
+    content +=
+        "\t\t\tvar ${moduleName.toLowerCase()} = JsonSerializable.fromMap<$className>(args.data, excludes:['id']);\n";
+    content += "\t\t\t\n";
+    content +=
+        "\t\t\tvar coll = mongo.collection('${moduleName.toLowerCase()}');\n";
+    content += "\t\t\t\n";
+    content +=
+        "\t\t\tfinal result = await coll.insert(${moduleName.toLowerCase()}.toMap());\n";
+    content += "\t\t\t\n";
+    content += "\t\t\tif (result.isNotEmpty) {\n";
+    content +=
+        "\t\t\t\treturn HttpResponse.ok(jsonEncode(${moduleName.toLowerCase()}.toMap()));\n";
+    content += "\t\t\t}\n";
+    content += "\t\t\t\n";
+    content +=
+        "\t\t\tthrow Exception('Failed at insert new ${moduleName.toLowerCase()}');\n";
 
-    content += "\n\t$className({this.id, $constructorFields}) {\n";
-    content += "\t\tid ??= Uuid().v4();\n";
+    content += "\t\t} on Exception catch (e) {\n";
+    content +=
+        "\t\t\treturn HttpResponse.notFound(jsonEncode({'error': e.toString()}));\n";
+    content += "\t\t} finally {\n";
+    content += "\t\t\tawait app.config.disconnectMongo();\n";
+    content += "\t\t}\n";
     content += "\t}\n";
-    content += "}";
+    content += "}\n";
 
     var path =
-        "example/${moduleName.toLowerCase()}/${moduleName.toLowerCase()}_entity.dart";
+        "example/${moduleName.toLowerCase()}/usecases/create_${moduleName.toLowerCase()}.dart";
 
     var file = await File(path).create(recursive: true);
 
